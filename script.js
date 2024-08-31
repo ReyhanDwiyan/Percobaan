8942390d788994961affab4322c3ee8e2e9968b3
Adul <rereeaa56@gmail.com>
2024-08-31 14:37:12 +0700

    7

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-
 let currentCardIndex = 0;
-const cards = document.querySelectorAll('.card');
-const nextButtons = document.querySelectorAll('.nextBtn');
+const cards = document.querySelectorAll(".card");
+const nextButtons = document.querySelectorAll(".nextBtn");
 
 function showCard(index) {
-    cards.forEach((card, i) => {
-        card.classList.remove('show');
-        if (i === index) {
-            card.classList.add('show');
-        }
-    });
+  cards.forEach((card, i) => {
+    card.classList.remove("show");
+    if (i === index) {
+      card.classList.add("show");
+    }
+  });
 }
 
 nextButtons.forEach((button, index) => {
-    button.addEventListener('click', () => {
-        const nextIndex = (currentCardIndex + 1) % cards.length;
-        showCard(nextIndex);
-        currentCardIndex = nextIndex;
-    });
+  button.addEventListener("click", () => {
+    const nextIndex = (currentCardIndex + 1) % cards.length;
+    showCard(nextIndex);
+    currentCardIndex = nextIndex;
+  });
 });
 
 // Show the first card initially
