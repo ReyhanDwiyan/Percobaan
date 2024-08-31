55c0412caab543290ac06af24b1b29bbc80bf3c8
Adul <rereeaa56@gmail.com>
2024-08-31 14:33:17 +0700

    6

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-let currentCardIndex = -1;
+
+let currentCardIndex = 0;
 const cards = document.querySelectorAll('.card');
 const nextButtons = document.querySelectorAll('.nextBtn');
-const yesButton = document.querySelector('.yesBtn');
-const noButton = document.querySelector('.noBtn');
-const startButton = document.getElementById('startBtn');
 
 function showCard(index) {
     cards.forEach((card, i) => {
@@ -14,33 +12,12 @@ function showCard(index) {
     });
 }
 
-startButton.addEventListener('click', () => {
-    currentCardIndex = 0;
-    showCard(currentCardIndex);
-    startButton.style.display = 'none';
-});
-
-nextButtons.forEach((button) => {
+nextButtons.forEach((button, index) => {
     button.addEventListener('click', () => {
-        if (currentCardIndex < cards.length - 1) {
-            currentCardIndex++;
-            showCard(currentCardIndex);
-        }
+        const nextIndex = (currentCardIndex + 1) % cards.length;
+        showCard(nextIndex);
+        currentCardIndex = nextIndex;
     });
-});
-
-yesButton.addEventListener('click', () => {
-    currentCardIndex++;
-    showCard(currentCardIndex);
-});
-
-noButton.addEventListener('click', () => {
-    const randomX = Math.floor(Math.random() * 80) + 'vw';
-    const randomY = Math.floor(Math.random() * 80) + 'vh';
-    document.getElementById('card-4').style.position = 'absolute';
-    document.getElementById('card-4').style.left = randomX;
-    document.getElementById('card-4').style.top = randomY;
-    showCard(3);
 });
 
 // Show the first card initially
